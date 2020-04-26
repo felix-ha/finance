ae627c6ce3948f9be3a3f6ca314e60aac720de8d
Felix Hauer <hauerf98@gmail.com>
2020-04-26 16:57:58 +0200

    implemted a net wit a simple rnn and a sequence of dummy vectors as an input

--- neural_net_architectures.py
+++ neural_net_architectures.py
@@ -29,6 +29,36 @@ class FC_2_Layers_Binary_Output(nn.Module):
             output = self.fc3(relu2)
             output = self.sigmoid(output)
             return output
+        
+        
+        
+class RNN(nn.Module):
+    def __init__(self, input_size, seq_len, output_size, hidden_dim, n_layers):
+        super(RNN, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.seq_len = seq_len
+        
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers)
+        self.fc = nn.Linear(hidden_dim, output_size)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, hidden):
+
+        # get RNN outputs, hidden is  unused
+        r_out, hidden = self.rnn(x, hidden)
+
+        
+        # shape output to be (batch_size*seq_length, hidden_dim)
+        r_out = r_out[:, self.seq_len-1]
+
+        # get final output
+        r_out = self.fc(r_out)
+        
+        output = self.sigmoid(r_out)
+        
+
+        return output
            
         
 def training_full_batch_SGD(model, X_train_T, y_train_T, lr,  n_epochs):
@@ -41,6 +71,24 @@ def training_full_batch_SGD(model, X_train_T, y_train_T, lr,  n_epochs):
         optimizer.zero_grad()
         # Forward pass
         y_pred = model(X_train_T)
+        # Compute Loss
+        loss = criterion(y_pred.squeeze(), y_train_T)
+       
+        print('Epoch {}: train loss: {}'.format(epoch, loss.item()))
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+        
+def training_full_batch_SGD_RNN(model, X_train_T, y_train_T, hidden_0, lr,  n_epochs):
+    criterion = nn.BCELoss()
+    optimizer = optim.SGD(model.parameters(), lr = lr)   
+   
+    
+    model.train()
+    for epoch in range(n_epochs):
+        optimizer.zero_grad()
+        # Forward pass
+        y_pred = model(X_train_T, hidden_0)
         # Compute Loss
         loss = criterion(y_pred.squeeze(), y_train_T)
        
---
+++ rnn_dummy_vectors.py
@@ -0,0 +1,163 @@
+"""
+Simple RNN Cell with sequence of on hot dummy vectors
+
+"""
+
+import pandas as pd
+import numpy as np
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+
+import re
+from bs4 import BeautifulSoup
+
+from sklearn.model_selection import train_test_split
+from neural_net_architectures import training_full_batch_SGD_RNN, RNN
+from torch import from_numpy, zeros, tensor
+
+from validation import run_validation
+
+
+def sentence_to_words(review):
+    nltk.download("stopwords", quiet=True)
+    stemmer = PorterStemmer()
+    
+    text = BeautifulSoup(review, "html.parser").get_text() # Remove HTML tags
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) # Convert to lower case
+    words = text.split() # Split string into words
+    words = [w for w in words if w not in stopwords.words("english")] # Remove stopwords
+    words = [PorterStemmer().stem(w) for w in words] # stem
+    
+    return words
+
+def build_dict(data, vocab_size):
+    """Construct and return a dictionary mapping each of the most frequently appearing words to a unique integer."""
+    
+    # TODO: Determine how often each word appears in `data`. Note that `data` is a list of sentences and that a
+    #       sentence is a list of words.
+    
+    train_X_flat = [elem for list in data for elem in list]
+    word_count = dict(Counter(train_X_flat)) # A dict storing the words that appear in the reviews along with how often they occur
+    
+    # TODO: Sort the words found in `data` so that sorted_words[0] is the most frequently appearing word and
+    #       sorted_words[-1] is the least frequently appearing word.
+    
+    sorted_words = sorted(word_count, key=word_count.get, reverse=True)
+    
+    word_dict = {} # This is what we are building, a dictionary that translates words into integers
+    for idx, word in enumerate(sorted_words[:vocab_size - 2]): # The -2 is so that we save room for the 'no word'
+        word_dict[word] = idx + 2                              # 'infrequent' labels
+        
+    return word_dict
+
+
+def one_hot_encode(arr, n_labels):
+    # Initialize the the encoded array
+    one_hot = np.zeros((np.multiply(*arr.shape), n_labels), dtype=np.float32)
+
+    # Fill the appropriate elements with ones
+    one_hot[np.arange(one_hot.shape[0]), arr.flatten()] = 1.
+
+    # Finally reshape it to get back to the original array
+    one_hot = one_hot.reshape((*arr.shape, n_labels))            
+    
+    return one_hot
+
+
+def convert_and_pad(word_dict, sentence, pad):
+    NOWORD = 0 # We will use 0 to represent the 'no word' category
+    INFREQ = 1 # and we use 1 to represent the infrequent words, i.e., words not appearing in word_dict
+    
+    working_sentence = [NOWORD] * pad
+    
+    for word_index, word in enumerate(sentence[:pad]):
+        if word in word_dict:
+            working_sentence[word_index] = word_dict[word]
+        else:
+            working_sentence[word_index] = INFREQ
+            
+    return working_sentence, min(len(sentence), pad)
+
+def convert_and_pad_data(word_dict, data, pad):
+    result = []
+    lengths = []
+    
+    for sentence in data:
+        converted, leng = convert_and_pad(word_dict, sentence, pad)
+        result.append(converted)
+        lengths.append(leng)
+        
+    return np.array(result), np.array(lengths)
+
+# test implementation on imdb data set
+df = pd.read_pickle('temp/imdb.pkl')
+
+# use reddit data
+#df = pd.read_pickle('temp/data.pkl')
+
+
+seq_len = pad = 100
+vocab_size = dummy_dim = 500
+
+print('transform sentences to words')
+_title_words = df["title"].apply(sentence_to_words)
+df = df.assign(words = _title_words)
+df.to_pickle(r'temp\data.pkl')
+df = pd.read_pickle('temp/data.pkl')
+
+
+print('build dictionary')
+word_dict = build_dict(df["words"].values, vocab_size)
+
+
+print('convert and padd')
+X, X_len = convert_and_pad_data(word_dict, df["words"].values, seq_len)
+y = df['Target'].values
+
+print("creating dummies")
+X = one_hot_encode(X, n_labels=vocab_size)
+
+N, seq_len, dummy_dim = X.shape
+
+input_size=dummy_dim
+hidden_dim=100
+n_layers=1
+output_size=1
+
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0, stratify=y)
+
+X_train_T = from_numpy(X_train).float()
+y_train_T = from_numpy(y_train).float()  
+X_val_T = from_numpy(X_val).float()
+
+
+
+hidden = zeros(1, seq_len, hidden_dim).float()
+
+
+model = RNN(input_size, seq_len, output_size=output_size, hidden_dim=hidden_dim, n_layers=n_layers)
+
+training_full_batch_SGD_RNN(model, X_train_T, y_train_T, lr=0.25, hidden_0=hidden, n_epochs=2)
+
+
+model.eval()
+y_prob_val = model.forward(X_val_T, hidden).view(-1).detach().numpy()
+y_prob_train = model.forward(X_train_T, hidden).view(-1).detach().numpy()
+
+
+df_result_val = pd.DataFrame(data = {'y_true': y_val, 'y_prob': y_prob_val})
+df_result_train = pd.DataFrame(data = {'y_true': y_train, 'y_prob': y_prob_train})
+df_result_val.to_pickle('temp/result_val.pkl')
+df_result_train.to_pickle('temp/result_train.pkl')
+
+run_validation(n_bins = 7)
+
+
+
+
+
+
+
