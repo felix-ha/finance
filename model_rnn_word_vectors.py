e73c8c8a97858ab2cc3d2b826c21bf2af9f485f9
Felix Hauer <hauerf98@gmail.com>
2020-05-02 14:37:08 +0200

    implemented LSTM

--- model_rnn_word_vectors.py
+++ model_rnn_word_vectors.py
@@ -9,9 +9,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.utils.random import sample_without_replacement
 
-from torch import from_numpy, zeros
+import torch
 
-from neural_net_architectures import training_SGD_RNN, RNN
+from neural_net_architectures import training_SGD_RNN, RNN, LSTM
 
 from validation import run_validation
 from data_handler import get_imdb_data, get_word2vec
@@ -28,9 +28,9 @@ n_layers=1
 output_size=1
 
 test_size = 0.25
-random_state = 124
+random_state = 123
 lr = 0.2
-n_epochs = 25
+n_epochs = 10
 batch_size = 125
 
 n_bins_calibration = 10
@@ -70,23 +70,41 @@ input_size=dummy_dim
 
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=random_state, stratify=y)
 
-X_train_T = from_numpy(X_train).float()
-y_train_T = from_numpy(y_train).float()  
-X_val_T = from_numpy(X_val).float()
-y_val_T = from_numpy(y_val).float()  
+X_train_T = torch.from_numpy(X_train).float()
+y_train_T = torch.from_numpy(y_train).float()  
+X_val_T = torch.from_numpy(X_val).float()
+y_val_T = torch.from_numpy(y_val).float() 
 
-hidden = zeros(1, seq_len, hidden_dim_rnn).float()
+ 
+# LSTM
+hidden_0 = torch.zeros(1, seq_len, hidden_dim_rnn)
+cell_0 = torch.zeros(1, seq_len, hidden_dim_rnn)
+hidden_cell_0 = (hidden_0, cell_0)
 
-model = RNN(input_size, seq_len, output_size=output_size, hidden_dim_rnn=hidden_dim_rnn,
+model = LSTM(input_size, seq_len, output_size=output_size, hidden_dim_rnn=hidden_dim_rnn,
             hidden_dim_fc=hidden_dim_fc, drop_p=drop_p, n_layers=n_layers)
 
 training_losses, valid_losses = training_SGD_RNN(model, X_train_T, y_train_T, X_val_T, y_val_T,
-                 lr=lr, hidden_0=hidden, n_epochs=n_epochs, batch_size=batch_size)
+                 lr=lr, hidden_0=hidden_cell_0, n_epochs=n_epochs, batch_size=batch_size)
+
+# RNN
+
+#hidden = zeros(1, seq_len, hidden_dim_rnn).float()
+
+#model = RNN(input_size, seq_len, output_size=output_size, hidden_dim_rnn=hidden_dim_rnn,
+#            hidden_dim_fc=hidden_dim_fc, drop_p=drop_p, n_layers=n_layers)
+
+#training_losses, valid_losses = training_SGD_RNN(model, X_train_T, y_train_T, X_val_T, y_val_T,
+#                 lr=lr, hidden_0=hidden, n_epochs=n_epochs, batch_size=batch_size)
+
+
+
 
 
 model.eval()
-y_prob_val = model.forward(X_val_T, hidden).view(-1).detach().numpy()
-y_prob_train = model.forward(X_train_T, hidden).view(-1).detach().numpy()
+with torch.no_grad():
+    y_prob_val = model.forward(X_val_T, hidden_cell_0).view(-1).detach().numpy()
+    y_prob_train = model.forward(X_train_T, hidden_cell_0).view(-1).detach().numpy()
 
 df_result_val = pd.DataFrame(data = {'y_true': y_val, 'y_prob': y_prob_val})
 df_result_train = pd.DataFrame(data = {'y_true': y_train, 'y_prob': y_prob_train})
--- neural_net_architectures.py
+++ neural_net_architectures.py
@@ -72,21 +72,37 @@ class RNN_last_output(nn.Module):
         self.hidden_dim = hidden_dim
         self.seq_len = seq_len
         
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers)
-        self.fc = nn.Linear(hidden_dim, output_size)
-        self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x, hidden):
-
-        r_out, hidden = self.rnn(x, hidden)
-
-        r_out = r_out[:, self.seq_len-1]
-
-        r_out = self.fc(r_out)
-        
+       
         output = self.sigmoid(r_out)
 
         return output
+class LSTM(nn.Module):
+    def __init__(self, input_size, seq_len, output_size,
+                 hidden_dim_rnn, hidden_dim_fc, n_layers, drop_p):
+        super(LSTM, self).__init__()
+        
+        self.rnn = nn.LSTM(input_size, hidden_dim_rnn, n_layers)
+        self.fc1 = nn.Linear(hidden_dim_rnn, hidden_dim_fc)
+        self.relu1 = nn.ReLU()
+        self.drop1 = nn.Dropout(p=drop_p)
+        self.fc2 = nn.Linear(hidden_dim_fc, output_size)
+        
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, hidden):
+        r_out, hidden = self.rnn(x, hidden)
+        r_out = torch.mean(r_out, dim=1)
+        hidden1 = self.fc1(r_out)
+        relu1 = self.relu1(hidden1)
+        drop1 = self.drop1(relu1)
+        output = self.fc2(drop1)
+
+        
+        output = self.sigmoid(output)
+
+        return output
+
            
     
 def training_SGD(model, X_train_T, y_train_T, X_val_T, y_val_T,
