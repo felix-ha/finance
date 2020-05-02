1026583028379a301a2225b57119e822720fc87c
Felix Hauer <hauerf98@gmail.com>
2020-05-02 13:59:52 +0200

    worked on understanding examples

--- lstm_understandig_dimensions.py
+++ lstm_understandig_dimensions.py
@@ -4,7 +4,7 @@ Demonstrating how a RNN cell works
 
 import numpy as np
 from torch import nn
-from torch import from_numpy, zeros
+from torch import from_numpy, zeros, mean
 
 
 class RNN_debug(nn.Module):
@@ -14,32 +14,36 @@ class RNN_debug(nn.Module):
         self.hidden_dim = hidden_dim
         self.seq_len = seq_len
         
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers)
+        self.lstm = nn.LSTM(input_size, hidden_dim, n_layers)
         self.fc = nn.Linear(hidden_dim, output_size)
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x, hidden):
-
+    def forward(self, x, hidden_cell):
 
         # get RNN outputs, hidden is  unused
-        r_out, hidden = self.rnn(x, hidden)
+        r_out, hidden_cell = self.lstm(x, hidden_cell)
+        hidden = hidden_cell[0]
+        cell = hidden_cell[1]
+
         
+        print("last hidden state of rnn cell: ")
+        print(hidden)
+        print("")
+        
+                
+        print("last cell state of rnn cell: ")
+        print(cell)
+        print("")
+        
+                
         print("output of rnn cell: ")
         print(r_out)
         print("")
         
-        print(" last hidden state of rnn cell: ")
-        print(hidden)
-        print("")
         
-
+        r_out = mean(r_out, dim=1)
         
-        
-        
-        # shape output to be (batch_size*seq_length, hidden_dim)
-        r_out = r_out[:, self.seq_len-1]
-        
-        print("selecting only last element of the outupt sequence, as this goes to the fc layer: ")
+        print("calculating elementwise mean of all output tensors, as this goes to the fc layer: ")
         print(r_out)
         print("")
 
@@ -94,7 +98,9 @@ output_size=1
 model = RNN_debug(input_size, seq_len, output_size=output_size, hidden_dim=hidden_dim, n_layers=n_layers)
 
 
-hidden = zeros(1, seq_len, hidden_dim)
+hidden_0 = zeros(1, seq_len, hidden_dim)
+cell_0 = zeros(1, seq_len, hidden_dim)
+hidden_cell_0 = (hidden_0, cell_0)
 
 
 print("N: ", N)
@@ -107,6 +113,5 @@ print("input to network: ")
 print(X_T)
 print("")
 
-y = model.forward(X_T, hidden)
-
+y = model.forward(X_T, hidden_cell_0)
 
